24d7836c881eec0773af0962432e272d17af9448
Danillo Vidal <vidaldanillo08@gmail.com>
2024-09-04 22:18:05 -0300

    função multiplicar (1)

--- menu.py
+++ menu.py
@@ -1,9 +1,7 @@
-def dividir():
-def subtrair():
+def multiplicar():
     num1 = float(input("Digite o primeiro número: "))
     num2 = float(input("Digite o segundo número: "))
-    print(f"Resultado: {num1 - num2}")
-
+    print(f"Resultado: {num1 * num2}")
 
 
 def menu():
